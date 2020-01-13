89f97d3744731ee746ec1a7630e1a8647f0ac59f
Ben Follington <5009316+bfollington@users.noreply.github.com>
2020-01-13 11:51:54 +1000

    Demo App that creates and lists projects

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
-const App: React.FC = () => {
+import API, { graphqlOperation } from "@aws-amplify/api";
+import PubSub from "@aws-amplify/pubsub";
+
+import { createProject } from "./graphql/mutations";
+import { listProjects } from "./graphql/queries";
+
+import awsconfig from "./aws-exports";
+import "./App.css";
+import { onCreateProject } from "./graphql/subscriptions";
+
+type Project = {
+  id?: string;
+  name: string;
+};
+
+// Configure Amplify
+API.configure(awsconfig);
+PubSub.configure(awsconfig);
+
+async function createNewProject(name: string) {
+  const project: Project = { name };
+  await API.graphql(graphqlOperation(createProject, { input: project }));
+}
+
+function App() {
+  const [projects, setProjects] = useState<Project[]>([]);
+  const [newProjectName, setNewProjectName] = useState("My Project");
+
+  useEffect(() => {
+    API.graphql(graphqlOperation(listProjects)).then((res: any) => {
+      console.log(res);
+      setProjects(res.data.listProjects.items);
+    });
+  }, []);
+
+  useEffect(() => {
+    const subscription = API.graphql(
+      graphqlOperation(onCreateProject)
+    ).subscribe({
+      next: (eventData: any) => {
+        const project = eventData.value.data.onCreateProject;
+        setProjects([...projects, project]);
+      }
+    });
+
+    return () => subscription.unsubscribe();
+  }, [projects]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        type="text"
+        value={newProjectName}
+        onChange={e => setNewProjectName(e.target.value)}
+      />
+      <button onClick={() => createNewProject(newProjectName)}>
+        Add Project
+      </button>
+      <ul>
+        {projects.map(p => (
+          <li key={p.id}>{p.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
